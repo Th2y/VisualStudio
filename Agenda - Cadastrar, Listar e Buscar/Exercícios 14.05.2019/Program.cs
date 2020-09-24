9640ef067cbb8751d642b840daa87ace0056d203
Thayane Carvalho dos Santos <56850377+Th2y@users.noreply.github.com>
2020-09-24 11:26:51 -0300

    Exercícios 14.05.2019

---
+++ Agenda - Cadastrar, Listar e Buscar/Exercícios 14.05.2019/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M.E._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Número = " + i);
+                numeros[i] = int.Parse(Console.ReadLine());
+            }
+            int Snumeros = numeros.Sum();
+            Console.WriteLine("A soma dos números é: " + Snumeros);
+
+            double Mnumeros = numeros.Average();
+            Console.WriteLine("A média dos números é: " + Mnumeros);
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
+
+
+
