d8f0e55afdff400b987a1cef38a6062d00c8c0fa
Thayane Carvalho dos Santos <56850377+Th2y@users.noreply.github.com>
2020-09-24 11:23:25 -0300

    Agenda - Cadastrar, Listar e Buscar

---
+++ Agenda - Cadastrar, Listar e Buscar/Agenda/Agenda/Agenda.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda
+{
+    class Agenda
+    {
+
+        Pessoa[] pessoas = new Pessoa[10];
+
+        public void Cadastrar(string nome, int idade, float altura)
+        {
+            for (int i = 0; i < pessoas.Length; i++)
+            {
+                if (pessoas[i] == null)
+                {
+                    // Adicionar uma pessoa
+                    Pessoa pessoa = new Pessoa(nome, idade, altura);
+                    pessoas[i] = pessoa;
+                    break;
+                }
+            }
+        }
+
+        public void Remover(string nome)
+        {
+            
+        }
+
+        public void Buscar(string nome)
+        {
+            for (int i = 0; i < this.pessoas.Length; i++)
+            {
+                if (pessoas[i] != null)
+                {
+                    if (pessoas[i].nome == nome)
+                    {
+                        Console.WriteLine(pessoas[i].nome + " " + pessoas[i].idade + " " + pessoas[i].altura);
+                    }
+                }
+            }
+        }
+
+        public void Listar()
+        {
+            for (int i = 0; i < this.pessoas.Length; i++)
+            {
+                if (pessoas[i] != null)
+                {
+                    Console.WriteLine(pessoas[i].nome + " " + pessoas[i].idade + " " + pessoas[i].altura);
+                }
+            }
+        }
+
+    }
+}
---
+++ Agenda - Cadastrar, Listar e Buscar/Agenda/Agenda/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Agenda agenda = new Agenda();
+            do
+            {
+                Console.WriteLine("**** Agenda ****");
+                Console.WriteLine();
+                Console.WriteLine("1) Cadastrar");
+                Console.WriteLine("2) Remover");
+                Console.WriteLine("3) Buscar");
+                Console.WriteLine("4) Listar");
+                Console.WriteLine();
+                Console.Write("Opção..: ");
+
+
+                int opcao = int.Parse(Console.ReadLine());
+                if (opcao == 1)
+                {
+                    // Cadastrar
+                    Console.WriteLine("Digite as informações da pessoa");
+                    Console.Write("Nome: ");
+                    string nome = Console.ReadLine();
+                    Console.Write("Idade: ");
+                    int idade = int.Parse(Console.ReadLine());
+                    Console.Write("Altura: ");
+                    float altura = float.Parse(Console.ReadLine());
+
+                    agenda.Cadastrar(nome, idade, altura);
+                    agenda.Listar();
+                }
+                else if (opcao == 2)
+                {
+                    // Remover
+                    Console.WriteLine("Digite as informações da pessoa");
+                    Console.Write("Nome: ");
+                    string nome = Console.ReadLine();
+                    agenda.Remover(nome);
+                }
+                else if (opcao == 3)
+                {
+                    // Buscar
+                    Console.WriteLine("Digite as informações da pessoa");
+                    Console.Write("Nome: ");
+                    string nome = Console.ReadLine();
+                    agenda.Buscar(nome);
+                }
+                else if (opcao == 4)
+                {
+                    // Listar
+                    agenda.Listar();
+                }
+
+            } while (true);
+        }
+    }
+}
