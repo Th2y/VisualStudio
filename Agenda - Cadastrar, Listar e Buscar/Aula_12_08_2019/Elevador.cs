a4b38c1105d7cf60bc7c5736987ff9e990b0a271
Thayane Carvalho dos Santos <56850377+Th2y@users.noreply.github.com>
2020-09-24 11:24:39 -0300

    Aula_12_08_2019

---
+++ Agenda - Cadastrar, Listar e Buscar/Aula_12_08_2019/Elevador.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_12_08_2019
+{
+    class Elevador
+    {
+        private int quantidadePessoas;
+        private int andarAtual;
+
+        private int maximoPessoas;
+        private int quantidadeAndares;
+
+        public Elevador(int quantidadeAndares, int maximoPessoas)
+        {
+            this.quantidadeAndares = quantidadeAndares;
+            this.maximoPessoas = maximoPessoas;
+            this.quantidadePessoas = 0;
+            this.andarAtual = 1;
+        }
+
+        public int QuantidadePessoas
+        {
+            get
+            {
+                return this.quantidadePessoas;
+            }
+            set
+            {
+                if (value <= maximoPessoas)
+                {
+                    if (value >= 0)
+                    {
+                        this.quantidadePessoas = value;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Elevador já está vazio.");
+                        Console.ResetColor();
+                    }
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Elevador lotado.");
+                    Console.ResetColor();
+                }
+            }
+        }
+
+        public int AndarAtual
+        {
+            get
+            {
+                return this.andarAtual;
+            }
+            set
+            {
+                if (value <= this.quantidadeAndares)
+                {
+                    if (value >= 1)
+                    {
+                        this.andarAtual = value;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("O elevador não pode descer mais.");
+                        Console.ResetColor();
+                    }
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("O elevador não pode subir mais.");
+                    Console.ResetColor();
+                }
+            }
+        }
+
+        public void Entra()
+        {
+            int pessoas = QuantidadePessoas;
+            QuantidadePessoas++;
+
+            if (pessoas < QuantidadePessoas)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("Uma pessoa entrou no elevador.");
+                Console.ResetColor();
+            }
+        }
+        public void Sai()
+        {
+            int pessoas = QuantidadePessoas;
+            QuantidadePessoas--;
+
+            if (pessoas > QuantidadePessoas)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("Uma pessoa saiu do elevador.");
+                Console.ResetColor();
+            }
+        }
+
+        public void Sobe()
+        {
+            int andar = AndarAtual;
+            AndarAtual++;
+
+            if (andar < AndarAtual)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("O elevador subiu um andar.");
+                Console.ResetColor();
+            }
+        }
+
+        public void Desce()
+        {
+            int andar = AndarAtual;
+            AndarAtual--;
+
+            if (andar > AndarAtual)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("O elevador desceu um andar.");
+                Console.ResetColor();
+            }
+        }
+    }
+}
---
+++ Agenda - Cadastrar, Listar e Buscar/Aula_12_08_2019/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_12_08_2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = true;
+
+            Console.WriteLine();
+            Console.WriteLine("Informações do elevador");
+            Console.WriteLine("-----------------------");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("Qual a quantidade máxima de andares: ");
+            int quantidadeAndares = int.Parse(Console.ReadLine());
+
+            Console.Write("Qual a quantidade máxima de pessoas: ");
+            int maximoPessoas = int.Parse(Console.ReadLine());
+
+            Elevador elevador = new Elevador(quantidadeAndares, maximoPessoas);
+            
+            string[] opcoes = new string[] { "Entrar", "Sair", "Subir", "Descer" };
+            int opcao = 0;
+            Console.CursorVisible = false;
+            while(true)
+            {
+                Console.Clear();
+
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("Quantidade atual de pessoas: ");
+                Console.WriteLine(elevador.QuantidadePessoas);
+
+                Console.Write("Andar atual: ");
+                Console.WriteLine(elevador.AndarAtual);
+                Console.WriteLine();
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write("Utilizando as setas, selecione uma das opções abaixo:");
+                Console.WriteLine();
+
+                for (int i = 0; i < opcoes.Length; i++)
+                {
+                    if (opcao == i)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.Write("[ ");
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.Write(opcoes[i]);
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.WriteLine(" ]");
+                    } else
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                        Console.WriteLine(opcoes[i]);
+                    }                    
+                }
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                ConsoleKeyInfo keyInfo = Console.ReadKey(false);
+                Console.ResetColor();
+
+                if (keyInfo.Key == ConsoleKey.UpArrow)
+                {
+                    if (opcao > 0)
+                    {
+                        opcao--;
+                    }
+                } else
+                {
+                    if (keyInfo.Key == ConsoleKey.DownArrow)
+                    {
+                        if (opcao < (opcoes.Length - 1))
+                        {
+                            opcao++;
+                        }
+                    } else
+                    {
+                        if (keyInfo.Key == ConsoleKey.Enter)
+                        {
+                            switch(opcao)
+                            {
+                                case 0:
+                                    elevador.Entra();
+                                    break;
+                                case 1:
+                                    elevador.Sai();
+                                    break;
+                                case 2:
+                                    elevador.Sobe();
+                                    break;
+                                case 3:
+                                    elevador.Desce();
+                                    break;
+                                default:
+                                    Console.ForegroundColor = ConsoleColor.Red;                                    
+                                    Console.WriteLine("\n\nOpção inválida.");
+                                    Console.ResetColor();
+                                    break;
+                            }
+
+                            Console.WriteLine();
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.WriteLine("Pressione qualquer tecla para continuar");
+                            Console.ReadKey();
+                            Console.Clear();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
